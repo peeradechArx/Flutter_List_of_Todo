e5728d372fab5a26f33f3bae067d988deb43aabb
peeradechArx <96216541+peeradechArx@users.noreply.github.com>
2024-09-06 15:29:18 +0700

    1

---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
+        useMaterial3: true,
+      ),
+      home: const TodaApp(),
+    );
+  }
+}
+
+class TodaApp extends StatefulWidget {
+  const TodaApp({
+    super.key,
+  });
+
+  @override
+  State<TodaApp> createState() => _TodaAppState();
+}
+
+class _TodaAppState extends State<TodaApp> {
+  late TextEditingController _titleController;
+  late TextEditingController _detailController;
+  final List<Map<String, String>> _myList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _titleController = TextEditingController();
+    _detailController = TextEditingController();
+  }
+
+  void addTodoHandle(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text("Add new task"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: _titleController,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Title"),
+                ),
+                const SizedBox(height: 10),
+                TextField(
+                  controller: _detailController,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Detail"),
+                ),
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    setState(() {
+                      _myList.add({
+                        'title': _titleController.text,
+                        'detail': _detailController.text,
+                      });
+                    });
+                    _titleController.text = "";
+                    _detailController.text = "";
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Save"))
+            ],
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("List of Todo"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: ListView.builder(
+        itemCount: _myList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(
+                 'Title: ${_myList[index]['title'] ?? ""}',
+                style: const TextStyle(fontSize: 20), // ขนาดตัวอักษรของ title
+                ),
+                subtitle: Text(
+                 'Detail: ${_myList[index]['detail'] ?? ""}',
+                 style: const TextStyle(fontSize: 18), // ขนาดตัวอักษรของ subtitle
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          addTodoHandle(context);
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
